cf65d6d0533b01cec838ee2f91a8d4b366af3730
Options <Options@DESKTOP-3KTSES2>
2026-07-06 23:04:58 +0300

    session02

---
+++ ECommerce.Application/Contracts/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Application.Common;
+using ECommerce.Application.DTOs.ProductDtos;
+using ECommerce.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Contracts
+{
+    public interface IProductService
+    {
+        Task<Result<IReadOnlyList<ProductDto>>> GetAllProductAsync(CancellationToken ct = default);
+    }
+}
